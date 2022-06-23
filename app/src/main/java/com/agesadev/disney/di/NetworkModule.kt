c33b47dc2dc14f4beea9479b4ad0d2e17a6d9ef9
agesa3 <collinsagesa2000@gmail.com>
2022-06-23 17:10:48 +0300

    added htp interceptor

--- app/src/main/java/com/agesadev/disney/di/NetworkModule.kt
+++ app/src/main/java/com/agesadev/disney/di/NetworkModule.kt
@@ -1,13 +1,17 @@
 package com.agesadev.disney.di
 
+import com.agesadev.disney.BuildConfig
 import com.agesadev.disney.data.remote.retrofit.CharacterApi
 import com.agesadev.disney.utils.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
@@ -15,12 +19,28 @@ import javax.inject.Singleton
 @Module
 object NetworkModule {
 
+    //logging interceptor
+    @Provides
+    @Singleton
+     fun provideOkHttpClient() =
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+        } else {
+            OkHttpClient.Builder().build()
+        }
+
+
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
             .build()
     }
 
@@ -30,7 +50,6 @@ object NetworkModule {
     fun provideCharacterApiClient(retrofit: Retrofit): CharacterApi {
         return retrofit.create(CharacterApi::class.java)
     }
-
 
 
 }
--- app/src/main/java/com/agesadev/disney/domain/use_case/GetAllCharactersUseCase.kt
+++ app/src/main/java/com/agesadev/disney/domain/use_case/GetAllCharactersUseCase.kt
@@ -1,13 +1,17 @@
 package com.agesadev.disney.domain.use_case
 
+import androidx.paging.PagingData
+import com.agesadev.disney.domain.model.Character
 import com.agesadev.disney.domain.repository.CharacterRepository
 import com.agesadev.disney.utils.Resource
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
 class GetAllCharactersUseCase @Inject constructor(private val characterRepository: CharacterRepository) {
+
     operator fun invoke() = flow {
         try {
             emit(Resource.Loading())
@@ -19,4 +23,5 @@ class GetAllCharactersUseCase @Inject constructor(private val characterRepositor
             emit(Resource.Error("Please check your internet connection."))
         }
     }
+
 }
